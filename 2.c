3334edc817bd6d676762aff17b3d5a063a7ae075
rohitmehra00 <createmeh90@gmail.com>
2024-11-28 18:52:06 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	
+	printf("enter no a");
+	scanf("%d",&a);
+	
+	printf("enter no b");
+	scanf("%d",&b);
+	
+	if (a<b)
+	{
+		printf (" a is min");
+    }
+    else
+    {
+    	printf("b is min");
+	}
+	int sum=0
+	{
+		printf("enter sum,%d",sum);
+	}
+	
+	
+	
+	
+}
+  
+	
